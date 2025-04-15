bf3f3b907335fccaf0a99c33594a701bf37dd6fd
Chumi90 <yabel_chumillas@hotmail.com>
2025-04-15 14:50:20 +0200

    Hours function

--- js/hours.js
+++ js/hours.js
@@ -4,22 +4,35 @@ let data=new Date();
 let datepresents=data.toLocaleDateString();
 let hourspresents=data.toLocaleTimeString();
 function timeInterval(){
-if(data.toLocaleTimeString()[1]==":"){
-    hourspresents="0"+hourspresents;
-}
-if(hourspresents[4]==":"){
-    hourspresents=hourspresents.slice(0,3)+"0"+hourspresents.slice(0,3);
-}
+    if(data.toLocaleTimeString()[1]==":"){
+        hourspresents="0"+hourspresents;
+    }
+    if(hourspresents[4]==":"){
+        hourspresents=hourspresents.slice(0,3)+"0"+hourspresents.slice(0,3);
+    }
+    hour.innerHTML=hourspresents;
 }
 function dateInterval(){
-if(datepresents[1]=="/"){
-    datepresents="0"+datepresents;
+    if(datepresents[1]=="/"){
+        datepresents="0"+datepresents;
+    }
+    if(datepresents[4]=="/"){
+        datepresents=datepresents.slice(0,3)+"0"+datepresents.slice(3);
+    }
+    date.innerHTML=datepresents;
 }
-if(datepresents[4]=="/"){
-    datepresents=datepresents.slice(0,3)+"0"+datepresents.slice(3);
+
+function actualHour(){
+    data=new Date();
+    datepresents=data.toLocaleDateString();
+    hourspresents=data.toLocaleTimeString();
 }
-}
+
 timeInterval();
 dateInterval();
-console.log(hourspresents);
-console.log(datepresents);
+
+setInterval(()=>{
+    actualHour();
+    timeInterval();
+    dateInterval();
+},1000)
