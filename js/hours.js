1f8e1ef9fd8eb3e64218e63521485e09b1e7662a
Chumi90 <yabel_chumillas@hotmail.com>
2025-04-15 18:36:40 +0200

    Hours finish

--- js/hours.js
+++ js/hours.js
@@ -1,8 +1,10 @@
 const hour=document.getElementById("Time");
 const date=document.getElementById("Date");
+const message=document.getElementById("Mensaje");
 let data=new Date();
 let datepresents=data.toLocaleDateString();
 let hourspresents=data.toLocaleTimeString();
+
 function timeInterval(){
     if(data.toLocaleTimeString()[1]==":"){
         hourspresents="0"+hourspresents;
@@ -28,11 +30,59 @@ function actualHour(){
     hourspresents=data.toLocaleTimeString();
 }
 
+function mensajeUsuario(){
+    let mensaje="";
+    let hora=hourspresents.slice(0,2);
+    let minuto=hourspresents.slice(3,5);
+    //console.log(hora)
+    //console.log(minuto)
+    switch(hora){
+        case "00":
+            if (minuto>=1){
+            mensaje="Es hora de descansar. Apaga y sigue mañana";
+            }
+            break;
+        case "07":
+            if (minuto>=1){
+                mensaje="Buenos días, desayuna fuerte y a darle al código";
+            }
+            break;
+        case "12":
+            if (minuto>=1){
+                mensaje="Echa un rato más pero no olvides comer";
+            }
+            break;
+        case "14":
+            if (minuto>=1){
+                mensaje="Espero que hayas comido";
+            }
+            break;
+        case "16":
+            if (minuto>=1){
+                mensaje="Buenas tardes, el último empujón";
+            }
+            break;
+        case "18":
+            if (minuto>=1){
+                mensaje="Esto ya son horas extras, ... piensa en parar pronto";
+            }
+            break;
+        case "22":
+            if (minuto>=1){
+                mensaje="Buenas noches, es hora de pensar en parar y descansar";
+            }
+            break;
+    }
+    message.innerHTML=mensaje;
+}
+
 timeInterval();
 dateInterval();
+mensajeUsuario();
 
 setInterval(()=>{
     actualHour();
     timeInterval();
+    mensajeUsuario();
     dateInterval();
 },1000)
